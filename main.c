4b5102a10575b91c5f1edbf2f5c6b518e91a5eb8
Hossam Limouni <hlimouni@e2r7p15.1337.ma>
2021-05-31 15:14:11 +0100

    adding functions prototypes in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hlimouni <hlimouni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 11:17:03 by hlimouni          #+#    #+#             */
-/*   Updated: 2021/03/20 17:38:46 by hlimouni         ###   ########.fr       */
+/*   Updated: 2021/05/31 15:08:04 by hlimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 
-extern int ft_strcmp(const char *s1, const char *s2);
-int ft_strcmp_c(const char *s1, const char *s2);
+extern	int ft_strcmp(const char *s1, const char *s2);
+extern	char *ft_strcpy(char * dst, const char * src);
+extern	size_t ft_strlen(const char *str);
+extern	size_t ft_write(int fildes, const void *buf, size_t nbyte);
+extern	size_t ft_read(int fildes, void *buf, size_t nbyte);
+extern	char	*ft_strdup(const char *str);
 
 int main(void)
 {
-	char *str = "alslkjfsalkj";
+	char *str = "The quick brown fox jumps over the lazy dog";
+	char *str1 = "alslkjfsalkj";
 	char *str2 = "alslkjfsA lkj";
 	printf("%d\n", ft_strcmp_c(str, str2));
 	printf("%d\n", ft_strcmp(str, str2));
